86b57c0b44d7a49ac1e8020626c20fc352973120
Jinsuk Kim <jinsukkim94@gmail.com>
2020-02-19 14:20:40 -0800

    added react-icons

--- src/screens/Tools.tsx
+++ src/screens/Tools.tsx
@@ -6,39 +6,77 @@
  * @flow
  */
 
-import React, { Component } from 'react';
-import { View, Text, TouchableOpacity, Alert, Image, Button } from 'react-native';
-
+import React, { Component } from "react";
+import {
+	View,
+	Text,
+	TouchableOpacity,
+	Alert,
+	Image,
+	Button
+} from "react-native";
 
 export default class Tools extends Component {
-  render() {
-    return (
-      <View style={{ flex: 1 }}>
-        <View style={{ flex: 1, flexDirection: "row" }}>
-          <View style={{ flex: 1, alignItems: "center" }}>
-            <View style={{ flex: 1, alignItems: "center" }}>
-              <TouchableOpacity style={{ height: 150, width: 120, marginTop: 40, backgroundColor: "#FFFFFF", zIndex: 2 }}></TouchableOpacity>
-              <Text style={{ fontSize: 18, marginTop: 10 }}>Currency</Text>
-            </View>
-            <View style={{ flex: 1, alignItems: "center" }}>
-              <TouchableOpacity style={{ height: 150, width: 120, marginTop: 40, backgroundColor: "#FFFFFF", zIndex: 2 }}>
-                {/*add react-icons to each button*/}
-              </TouchableOpacity>
-              <Text style={{ fontSize: 18, marginTop: 10 }}>Journal</Text>
-            </View>
-          </View>
-          <View style={{ flex: 1, alignItems: "center" }} >
-            <View style={{ flex: 1, alignItems: "center" }}>
-              <TouchableOpacity style={{ height: 150, width: 120, marginTop: 40, backgroundColor: "#FFFFFF", zIndex: 2 }}></TouchableOpacity>
-              <Text style={{ fontSize: 18, marginTop: 10 }}>Photos</Text>
-            </View>
-            <View style={{ flex: 1, alignItems: "center" }}>
-              <TouchableOpacity style={{ height: 150, width: 120, marginTop: 40, backgroundColor: "#FFFFFF", zIndex: 2 }}></TouchableOpacity>
-              <Text style={{ fontSize: 18, marginTop: 10 }}>Todos</Text>
-            </View>
-          </View>
-        </View>
-      </View >
-    );
-  }
+	render() {
+		return (
+			<View style={{ flex: 1 }}>
+				<View style={{ flex: 1, flexDirection: "row" }}>
+					<View style={{ flex: 1, alignItems: "center" }}>
+						<View style={{ flex: 1, alignItems: "center" }}>
+							<TouchableOpacity
+								style={{
+									height: 150,
+									width: 120,
+									marginTop: 40,
+									backgroundColor: "#FFFFFF",
+									zIndex: 2
+								}}
+							></TouchableOpacity>
+							<Text style={{ fontSize: 18, marginTop: 10 }}>Currency</Text>
+						</View>
+						<View style={{ flex: 1, alignItems: "center" }}>
+							<TouchableOpacity
+								style={{
+									height: 150,
+									width: 120,
+									marginTop: 40,
+									backgroundColor: "#FFFFFF",
+									zIndex: 2
+								}}
+							>
+								{/*add react-icons to each button*/}
+							</TouchableOpacity>
+							<Text style={{ fontSize: 18, marginTop: 10 }}>Journal</Text>
+						</View>
+					</View>
+					<View style={{ flex: 1, alignItems: "center" }}>
+						<View style={{ flex: 1, alignItems: "center" }}>
+							<TouchableOpacity
+								style={{
+									height: 150,
+									width: 120,
+									marginTop: 40,
+									backgroundColor: "#FFFFFF",
+									zIndex: 2
+								}}
+							></TouchableOpacity>
+							<Text style={{ fontSize: 18, marginTop: 10 }}>Photos</Text>
+						</View>
+						<View style={{ flex: 1, alignItems: "center" }}>
+							<TouchableOpacity
+								style={{
+									height: 150,
+									width: 120,
+									marginTop: 40,
+									backgroundColor: "#FFFFFF",
+									zIndex: 2
+								}}
+							></TouchableOpacity>
+							<Text style={{ fontSize: 18, marginTop: 10 }}>Todos</Text>
+						</View>
+					</View>
+				</View>
+			</View>
+		);
+	}
 }
--- src/stacks/stacks.js
+++ src/stacks/stacks.js
@@ -1,107 +1,108 @@
-import { Navigation } from 'react-native-navigation';
-import { HOME, CALENDAR, TOOLS } from '../globals/screen';
-import { Home, Calendar, Tools } from '../screens/index';
+import { Navigation } from "react-native-navigation";
+import { HOME, CALENDAR, TOOLS } from "../globals/screen";
+import { Home, Calendar, Tools } from "../screens/index";
+
 // Register Each Screen to the Navigator by Assigning a name
 Navigation.registerComponent(HOME, () => Home);
 Navigation.registerComponent(CALENDAR, () => Calendar);
 Navigation.registerComponent(TOOLS, () => Tools);
 
 const mapStack = {
-  stack: {
-    children: [
-      {
-        component: {
-          name: HOME,
-          options: {
-            topBar: {
-              visible: true,
-              title: {
-                text: 'Map/Home'
-              },
-              rightButtons: [
-                {
-                  id: 'hi_button_id',
-                  text: 'Home'
-                }
-              ]
-            }
-          }
-        }
-      }
-    ],
-    options: {
-      bottomTab: {
-        text: 'Home',
-        icon: require('../images/image001.jpg')
-      }
-    }
-  }
+	stack: {
+		children: [
+			{
+				component: {
+					name: HOME,
+					options: {
+						topBar: {
+							visible: true,
+							title: {
+								text: "Map/Home"
+							},
+							rightButtons: [
+								{
+									id: "hi_button_id",
+									text: "Home"
+								}
+							]
+						}
+					}
+				}
+			}
+		],
+		options: {
+			bottomTab: {
+				text: "Home",
+				icon: require("../images/image001.jpg")
+			}
+		}
+	}
 };
 
 // Calendar View
 const calendarStack = {
-  stack: {
-    children: [
-      {
-        component: {
-          name: CALENDAR,
-          options: {
-            topBar: {
-              visible: true,
-              title: {
-                text: 'Calendar'
-              },
-              rightButtons: [
-                {
-                  id: 'hi_button_id',
-                  text: 'Calendar'
-                }
-              ]
-            }
-          }
-        }
-      }
-    ],
-    options: {
-      bottomTab: {
-        text: 'Calendar',
-        icon: require('../images/image001.jpg')
-      }
-    }
-  }
+	stack: {
+		children: [
+			{
+				component: {
+					name: CALENDAR,
+					options: {
+						topBar: {
+							visible: true,
+							title: {
+								text: "Calendar"
+							},
+							rightButtons: [
+								{
+									id: "hi_button_id",
+									text: "Calendar"
+								}
+							]
+						}
+					}
+				}
+			}
+		],
+		options: {
+			bottomTab: {
+				text: "Calendar",
+				icon: require("../images/image001.jpg")
+			}
+		}
+	}
 };
 
 // List of Tools
 const toolStack = {
-  stack: {
-    children: [
-      {
-        component: {
-          name: TOOLS,
-          options: {
-            topBar: {
-              visible: true,
-              title: {
-                text: 'Tools'
-              },
-              rightButtons: [
-                {
-                  id: 'hi_button_id',
-                  text: 'tools'
-                }
-              ]
-            }
-          }
-        }
-      }
-    ],
-    options: {
-      bottomTab: {
-        text: 'Tools',
-        icon: require('../images/image001.jpg')
-      }
-    }
-  }
+	stack: {
+		children: [
+			{
+				component: {
+					name: TOOLS,
+					options: {
+						topBar: {
+							visible: true,
+							title: {
+								text: "Tools"
+							},
+							rightButtons: [
+								{
+									id: "hi_button_id",
+									text: "tools"
+								}
+							]
+						}
+					}
+				}
+			}
+		],
+		options: {
+			bottomTab: {
+				text: "Tools",
+				icon: require("../images/image001.jpg")
+			}
+		}
+	}
 };
 
 export { mapStack, calendarStack, toolStack };
